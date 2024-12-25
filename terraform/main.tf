351b92a5059d6503fceaac739bb5008fa61f0e60
ambikac <ambikac@localhost.localdomain>
2024-12-25 19:34:15 +0530

    changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,19 +6,24 @@ resource "aws_instance" "instance-1" {
     ami = "ami-01816d07b1128cd2d"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "ambika"
+    key_name = aws_key_pair.my_key_pair.key_name
     user_data = file("grafana.sh")
     tags = {
         Name = "grafana-server"
     }
+
 }
 
 resource "aws_instance" "instance-2" {
     ami = "ami-01816d07b1128cd2d"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "ambika"
+     key_name = aws_key_pair.my_key_pair.key_name
     tags = {
         Name = "Node-expo"
     }
+}
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.key_name
+  public_key = file("${abspath(path.cwd)}/id_rsa_pub")
 }
